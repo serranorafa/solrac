0344f636e7a6997b3c928a702efd2b8a358b115f
Rafael Serrano Aracil <rsa26@alu.ua.es>
2019-10-05 19:13:14 +0200

    Agregar archivos de proyecto.

---
+++ solrac/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace solrac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelORKnosp = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.labelDiv = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelCSD = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.labelORCSD = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.labelORSuma = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.labelPR = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelErrorKnosp = new System.Windows.Forms.Label();
+            this.labelErrorDiam = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Knosp:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(47, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(257, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "OR:";
+            // 
+            // labelORKnosp
+            // 
+            this.labelORKnosp.AutoSize = true;
+            this.labelORKnosp.Location = new System.Drawing.Point(291, 40);
+            this.labelORKnosp.Name = "labelORKnosp";
+            this.labelORKnosp.Size = new System.Drawing.Size(10, 13);
+            this.labelORKnosp.TabIndex = 3;
+            this.labelORKnosp.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Diámetro supraselar:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Diámetro diafragma:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(47, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(141, 107);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(47, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // labelDiv
+            // 
+            this.labelDiv.AutoSize = true;
+            this.labelDiv.Location = new System.Drawing.Point(291, 88);
+            this.labelDiv.Name = "labelDiv";
+            this.labelDiv.Size = new System.Drawing.Size(10, 13);
+            this.labelDiv.TabIndex = 9;
+            this.labelDiv.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(251, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "CSD:";
+            // 
+            // labelCSD
+            // 
+            this.labelCSD.AutoSize = true;
+            this.labelCSD.Location = new System.Drawing.Point(291, 114);
+            this.labelCSD.Name = "labelCSD";
+            this.labelCSD.Size = new System.Drawing.Size(10, 13);
+            this.labelCSD.TabIndex = 11;
+            this.labelCSD.Text = "-";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(244, 114);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Quintil:";
+            // 
+            // labelORCSD
+            // 
+            this.labelORCSD.AutoSize = true;
+            this.labelORCSD.Location = new System.Drawing.Point(354, 114);
+            this.labelORCSD.Name = "labelORCSD";
+            this.labelORCSD.Size = new System.Drawing.Size(10, 13);
+            this.labelORCSD.TabIndex = 13;
+            this.labelORCSD.Text = "-";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(322, 114);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(26, 13);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "OR:";
+            // 
+            // labelORSuma
+            // 
+            this.labelORSuma.AutoSize = true;
+            this.labelORSuma.Location = new System.Drawing.Point(230, 176);
+            this.labelORSuma.Name = "labelORSuma";
+            this.labelORSuma.Size = new System.Drawing.Size(10, 13);
+            this.labelORSuma.TabIndex = 15;
+            this.labelORSuma.Text = "-";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(158, 176);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(53, 13);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "OR Total:";
+            // 
+            // labelPR
+            // 
+            this.labelPR.AutoSize = true;
+            this.labelPR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPR.Location = new System.Drawing.Point(291, 325);
+            this.labelPR.Name = "labelPR";
+            this.labelPR.Size = new System.Drawing.Size(14, 17);
+            this.labelPR.TabIndex = 17;
+            this.labelPR.Text = "-";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(63, 325);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(220, 17);
+            this.label15.TabIndex = 16;
+            this.label15.Text = "Posibilidad de resección (%):";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(165, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelErrorKnosp
+            // 
+            this.labelErrorKnosp.AutoSize = true;
+            this.labelErrorKnosp.ForeColor = System.Drawing.Color.Red;
+            this.labelErrorKnosp.Location = new System.Drawing.Point(138, 56);
+            this.labelErrorKnosp.Name = "labelErrorKnosp";
+            this.labelErrorKnosp.Size = new System.Drawing.Size(145, 13);
+            this.labelErrorKnosp.TabIndex = 19;
+            this.labelErrorKnosp.Text = "* Valor de entrada incorrecto.";
+            this.labelErrorKnosp.Visible = false;
+            // 
+            // labelErrorDiam
+            // 
+            this.labelErrorDiam.AutoSize = true;
+            this.labelErrorDiam.ForeColor = System.Drawing.Color.Red;
+            this.labelErrorDiam.Location = new System.Drawing.Point(138, 130);
+            this.labelErrorDiam.Name = "labelErrorDiam";
+            this.labelErrorDiam.Size = new System.Drawing.Size(145, 13);
+            this.labelErrorDiam.TabIndex = 20;
+            this.labelErrorDiam.Text = "* Valor de entrada incorrecto.";
+            this.labelErrorDiam.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(426, 378);
+            this.Controls.Add(this.labelErrorDiam);
+            this.Controls.Add(this.labelErrorKnosp);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelPR);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.labelORSuma);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.labelORCSD);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.labelCSD);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.labelDiv);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelORKnosp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Coeficiente Supraselar Diafragma (CSD)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelORKnosp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label labelDiv;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelCSD;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label labelORCSD;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label labelORSuma;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label labelPR;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelErrorKnosp;
+        private System.Windows.Forms.Label labelErrorDiam;
+    }
+}
+
---
+++ solrac/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace solrac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkTextBoxes())
+            {
+                int knosp = int.Parse(textBox1.Text);
+                double or_knosp = calculaORKnosp(knosp);
+
+                double diamSupraselar = double.Parse(textBox2.Text);
+                double diamDiafragma = double.Parse(textBox3.Text);
+                double div = diamSupraselar / diamDiafragma;
+
+                int csd = calculaCSD(div);
+                double or_csd = calculaORCSD(csd);
+                double or_total = or_knosp + or_csd;
+                double posibResec = calculaPosibResec(or_total);
+
+                labelORKnosp.Text = or_knosp.ToString();
+                labelDiv.Text = (Math.Truncate(div * 100) / 100).ToString();
+                labelCSD.Text = csd.ToString();
+                labelORCSD.Text = or_csd.ToString();
+                labelORSuma.Text = or_total.ToString();
+                if (posibResec == -1.0)
+                    labelPR.Text = "ERROR PR";
+                else
+                    labelPR.Text = posibResec.ToString();
+            }
+            else
+            {
+                labelPR.Text = "ERROR";
+            }
+        }
+
+        private bool checkTextBoxes()
+        {
+            return checkKnosp() && checkDiam();
+        }
+
+        private bool checkDiam()
+        {
+            // sustituye punto por coma decimal
+            string str = textBox2.Text;
+            textBox2.Text = str.Replace('.', ',');
+            str = textBox3.Text;
+            textBox3.Text = str.Replace('.', ',');
+            // comprueba que es un double
+            double diam1, diam2 = 0.0;
+            bool ok = double.TryParse(textBox2.Text, out diam1) && double.TryParse(textBox3.Text, out diam2);
+            if (diam1 < 0 || diam2 <= 0)
+                ok = false;
+
+            labelErrorDiam.Visible = !ok;
+            return ok;
+        }
+
+        private bool checkKnosp()
+        {
+            // comprueba que es un entero
+            int knosp;
+            bool ok = int.TryParse(textBox1.Text, out knosp);            
+            // comprueba que es un valor correcto
+            if (knosp < 0 || knosp > 4)
+                ok = false;
+
+            labelErrorKnosp.Visible = !ok;
+            return ok;
+        }
+
+        private double calculaPosibResec(double n)
+        {
+            double pr;
+            Console.WriteLine("¿n == 54.6? ¿" + n + " == " + 54.6 + "? " + (n == 54.6));
+            if (n == 54.6)
+                return 85.4;
+            switch (n)
+            {
+                case 2:
+                    pr = 58.4;
+                    break;
+                case 3.3:
+                    pr = 60.0;
+                    break;
+                case 6.1:
+                case 6.2:
+                    pr = 60.8;
+                    break;
+                case 7.4:
+                    pr = 68.2;
+                    break;
+                case 10.3:
+                    pr = 73.3;
+                    break;
+                case 36.5:
+                    pr = 78.2;
+                    break;
+                case 40.6:
+                    pr = 79.6;
+                    break;
+                case 53.3:
+                    pr = 80.9;
+                    break;
+                case 54.6:
+                    pr = 85.4;
+                    break;
+                case 57.5:
+                    pr = 91.2;
+                    break;
+                case 87.8:
+                    pr = 95.8;
+                    break;
+                case 166.5:
+                    pr = 94.4;
+                    break;
+                case 170.6:
+                    pr = 94.1;
+                    break;
+                case 217.8:
+                    pr = 92.3;
+                    break;
+                default:
+                    pr = -1.0;
+                    break;
+            }
+            return pr;
+        }
+
+        private double calculaORCSD(int csd)
+        {
+            double or;
+            switch (csd) {
+                case 1:
+                    or = 165.5;
+                    break;
+                case 2:
+                    or = 35.5;
+                    break;
+                case 3:
+                    or = 5.2;
+                    break;
+                case 4:
+                    or = 2.3;
+                    break;
+                default:
+                    or = 1;
+                    break;
+            }
+            return or;
+        }
+
+        private int calculaCSD(double div)
+        {
+            int csd;
+            if (div >= 0 && div <= 0.23)
+                csd = 1;
+            else if (div <= 0.65)
+                csd = 2;
+            else if (div <= 0.83)
+                csd = 3;
+            else if (div <= 1.1)
+                csd = 4;
+            else
+                csd = 5;
+            return csd;
+        }
+
+        private double calculaORKnosp(int knosp)
+        {
+            double or;
+
+            if (knosp == 4)
+                or = 1;
+            else if (knosp == 3)
+                or = 5.1;
+            else if (knosp >= 0 && knosp <= 2)
+                or = 52.3;
+            else
+                or = -1.0;
+            return or;
+        }
+    }
+}
